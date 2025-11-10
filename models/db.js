6f81590590f320778a2576f1b5d043cce8ce4110
Deocleciano Kauan <139511025+Deo637@users.noreply.github.com>
2025-11-10 10:54:38 -0300

    Correção nas variáveis para produção

--- models/db.js
+++ models/db.js
@@ -1,24 +1,31 @@
-const { Sequelize } = require('sequelize');
+// Arquivo: models/db.js (VERSÃO FINAL DE PRODUÇÃO)
 
-const sequelize = new Sequelize('med_control', 'root', '123456', {
-    host: 'localhost',
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config()
+// Carrega o .env (só para testes locais)
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,   process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     timezone: '-03:00',
     dialectOptions: {
         dateStrings: true,
         typeCast: true
-require('dotenv').config();
-
 const sequelize = new Sequelize(
-    
+    // 1º: Procura a variável da Railway (MYSQLDATABASE)
+    // 2º: Se não encontrar, procura a variável local (DB_NAME)
     process.env.MYSQLDATABASE || process.env.DB_NAME,
     process.env.MYSQLUSER || process.env.DB_USER,
     process.env.MYSQLPASSWORD || process.env.DB_PASS,
     {
         host: process.env.MYSQLHOST || process.env.DB_HOST,
-        port: process.env.MYSQLPORT || 3306,
+        port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
         dialect: 'mysql',
-        timezone: '-03:00', 
+        timezone: '-03:00',
         dialectOptions: {
             dateStrings: true,
             typeCast: true
